96b735be056804af2baedeffbc1db0ab46c1e4ec
Chris Pelzer <chrispelzer@gmail.com>
2018-05-02 11:07:42 -0400

    add an emailaddress to the certificate and change to 2048

--- scripts/base.sh
+++ scripts/base.sh
@@ -36,12 +36,12 @@ O=Vaprobash
 localityName=New Haven
 commonName=$DOMAIN
 organizationalUnitName=VaproBash
-emailAddress=
+emailAddress=noreply@example.org
 "
 
 sudo mkdir -p "$SSL_DIR"
 
-sudo openssl genrsa -out "$SSL_DIR/xip.io.key" 1024
+sudo openssl genrsa -out "$SSL_DIR/xip.io.key" 2048
 sudo openssl req -new -subj "$(echo -n "$SUBJ" | tr "\n" "/")" -key "$SSL_DIR/xip.io.key" -out "$SSL_DIR/xip.io.csr" -passin pass:$PASSPHRASE
 sudo openssl x509 -req -days 365 -in "$SSL_DIR/xip.io.csr" -signkey "$SSL_DIR/xip.io.key" -out "$SSL_DIR/xip.io.crt"
 
